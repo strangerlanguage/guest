fe2f32fa2ddea8164491e9064be3b68b243d9e35
guest <3125667110@qq.com>
2024-12-07 18:19:33 +0800

    Implement POST request handler to return static response, TCP data handling not yet implemented

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::{
     collections::HashMap,
     io::{prelude::*, BufReader},
     net::{SocketAddr, TcpListener, TcpStream},
-    sync::{Arc, Mutex},
+    sync::{Arc, RwLock},
     thread,
 };
 
@@ -14,8 +14,6 @@ use std::{
 ///
 /// ```rust
 /// use guest_server::{Server,HttpResponse};
-/// use std::time::Duration;
-/// use std::thread;
 ///
 /// let mut server = Server::new();
 /// server.get("/", home);
@@ -30,34 +28,85 @@ use std::{
 /// # Description
 /// This simple example shows how to create a 'Server' instance.
 /// Registers a GET route and simulates an HTTP request to obtain the response.
+#[derive(Eq, PartialEq, Hash)]
+pub enum HttpMethod {
+    GET,
+    POST,
+}
+
+/// Represents the HTTP server.
 pub struct Server {
-    router: Arc<Mutex<HashMap<String, Arc<dyn Fn() -> HttpResponse + Send + Sync + 'static>>>>,
+    routes: Arc<
+        RwLock<
+            HashMap<(HttpMethod, String), Arc<dyn Fn() -> HttpResponse + Send + Sync + 'static>>,
+        >,
+    >,
 }
 
 impl Server {
     /// Creates and initializes a new server instance.
+    ///
+    /// # Returns
+    /// A new instance of `Server` with an empty route configuration.
     pub fn new() -> Self {
         Self {
-            router: Arc::new(Mutex::new(HashMap::new())),
+            routes: Arc::new(RwLock::new(HashMap::new())),
         }
+    }
+
+    /// Registers a route with a specific HTTP method, path, and handler.
+    ///
+    /// # Parameters
+    /// - 'method' : The HTTP method (GET, POST) for this route.
+    /// - 'path' : The route path (e.g., '/home').
+    /// - 'handler' : The closure that processes the request for this path.
+    pub fn route<F>(&mut self, method: HttpMethod, path: &str, handler: F)
+    where
+        F: Fn() -> HttpResponse + Send + Sync + 'static,
+    {
+        self.routes
+            .write()
+            .unwrap()
+            .insert((method, path.to_string()), Arc::new(handler));
     }
 
     /// Registers a GET route with a specified path and handler.
     ///
     /// # Parameters
     /// - 'path' : The route path to register, e.g., '/home'.
-    /// - 'handler' : The closure that processes the request for this path and returns an HttpResponse.
+    /// - 'handler' : The closure that processes the request for this path.
     pub fn get<F>(&mut self, path: &str, handler: F)
     where
         F: Fn() -> HttpResponse + Send + Sync + 'static,
     {
-        self.router
-            .lock()
-            .unwrap()
-            .insert(path.to_string(), Arc::new(handler));
+        self.route(HttpMethod::GET, path, handler);
     }
 
-    /// Starts the server and listens on a specified port.
+    /// Registers a POST route with a specified path and handler.
+    ///
+    /// # Parameters
+    /// - 'path' : The route path to register, e.g., '/submit'.
+    /// - 'handler' : The closure that processes the request for this path.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use guest_server::{Server,HttpResponse};
+    /// let mut server = Server::new();
+    /// server.post("/submit",submit);
+    /// fn submit()->HttpResponse{
+    ///     HttpResponse::new(200, Some("{\"key\":\"value\"}".to_string())).insert_header("Content-Type","application/json")
+    /// }
+    /// server.listener(80);
+    /// ```
+    pub fn post<F>(&mut self, path: &str, handler: F)
+    where
+        F: Fn() -> HttpResponse + Send + Sync + 'static,
+    {
+        self.route(HttpMethod::POST, path, handler);
+    }
+
+    /// Starts the server and listens for incoming connections on the specified port.
     ///
     /// # Parameters
     /// - 'port' : The port number to listen on.
@@ -69,10 +118,10 @@ impl Server {
         for stream in listener.incoming() {
             match stream {
                 Ok(stream) => {
-                    let router = Arc::clone(&self.router);
+                    let routes = Arc::clone(&self.routes);
                     // Handle each connection in a separate thread
                     thread::spawn(move || {
-                        let server = Server { router };
+                        let server = Server { routes };
                         server.handle_connection(stream);
                     });
                 }
@@ -81,7 +130,10 @@ impl Server {
         }
     }
 
-    // Handle the HTTP connection.
+    /// Handles the HTTP connection by reading the request and sending an appropriate response.
+    ///
+    /// # Parameters
+    /// - 'stream' : The TCP stream for the current connection.
     fn handle_connection(&self, mut stream: TcpStream) {
         let buf_reader = BufReader::new(&mut stream);
 
@@ -101,31 +153,48 @@ impl Server {
             let method = parts[0];
             let path = parts[1];
 
-            // Invalid request line
-            let not_found_response = HttpResponse::new(400, None);
-
-            // Lock router once and handle request
-            let response = match method {
-                "GET" => {
-                    let handler = self.router.lock().unwrap().get(path).cloned();
-                    handler.map(|h| h()).unwrap_or_else(|| not_found_response)
-                }
-                _ => not_found_response,
+            // Lock routes once and handle request
+            let method: Option<HttpMethod> = match method {
+                "GET" => Some(HttpMethod::GET),
+                "POST" => Some(HttpMethod::POST),
+                _ => None,
             };
+
+            let response = if let Some(method) = method {
+                self.routes
+                    .read()
+                    .unwrap()
+                    .get(&(method, path.to_string()))
+                    .cloned()
+                    .map_or_else(|| HttpResponse::new(404, None), |h| h())
+            } else {
+                HttpResponse::new(405, None) // Method Not Allowed
+            };
+
             let res = Self::generate_http_response(&response);
             // Send the response back to the client
             self.send_response(&mut stream, &res);
         }
     }
 
-    // Send an HTTP response to the client.
+    /// Sends an HTTP response to the client.
+    ///
+    /// # Parameters
+    /// - 'stream' : The TCP stream to send the response over.
+    /// - 'response' : The response content (HTTP status, headers, body).
     fn send_response(&self, stream: &mut TcpStream, response: &str) {
         if let Err(e) = stream.write_all(response.as_bytes()) {
             eprintln!("Failed to send response: {}", e);
         }
     }
 
-    // Generates the full HTTP response as a string, including headers and body.
+    /// Generates the full HTTP response string, including status code, headers, and body.
+    ///
+    /// # Parameters
+    /// - 'response' : The HttpResponse object containing status, headers, and body.
+    ///
+    /// # Returns
+    /// A string representing the full HTTP response.
     fn generate_http_response(response: &HttpResponse) -> String {
         let mut response_string = format!(
             "HTTP/1.1 {} {}\r\n",
@@ -145,6 +214,7 @@ impl Server {
     }
 }
 
+/// Represents an HTTP response, including status code, headers, and body.
 pub struct HttpResponse {
     pub status_code: u16,
     pub headers: HashMap<String, String>,
@@ -152,12 +222,11 @@ pub struct HttpResponse {
 }
 
 impl HttpResponse {
-    /// Create a default HttpResponse.
-    /// Defaults: Status code 200, headers Content-Type and Content-Length.
+    /// Creates a new HttpResponse with the specified status code and body.
     ///
     /// # Parameters
-    /// - 'status_code' : The request status code
-    /// - 'body' : The request body
+    /// - 'status_code' : The HTTP status code (e.g., 200, 404).
+    /// - 'body' : The response body content (optional).
     pub fn new(status_code: u16, body: Option<String>) -> Self {
         let mut headers = HashMap::new();
         let default_content_type = if let Some(ref b) = body {
@@ -184,23 +253,26 @@ impl HttpResponse {
     /// Adds or updates a single header field.
     ///
     /// # Parameters
-    /// - 'key' : The request header key
-    /// - 'value' : The request header value
+    /// - 'key' : The header key.
+    /// - 'value' : The header value.
     pub fn insert_header(mut self, key: &str, value: &str) -> Self {
         self.headers.insert(key.to_string(), value.to_string());
         self
     }
 
-    /// Add or overwrite header fields in batches.
+    /// Adds or updates multiple header fields in batch.
     ///
     /// # Parameters
-    /// - 'new_headers' : The request headers
+    /// - 'new_headers' : A HashMap containing the new header fields.
     pub fn insert_headers(mut self, new_headers: HashMap<String, String>) -> Self {
         self.headers.extend(new_headers);
         self
     }
 
-    /// Retrieves the description for the status code.
+    /// Retrieves the description message for the status code.
+    ///
+    /// # Returns
+    /// A string representing the status message for the given status code.
     pub fn get_status_message(&self) -> &'static str {
         match self.status_code {
             200 => "OK",
